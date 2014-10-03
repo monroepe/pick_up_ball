d06479d2f7a4a5d1728cab8584b6c52f4bf41786
Peter <monroepe@gmail.com>
2014-10-03 17:26:32 -0400

    search for park name

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,5 +2,14 @@ class Location < ActiveRecord::Base
   belongs_to :user
   has_many :games, dependent: :destroy
 
-  validates :name, :address, :city, :state, :zip_code, presence: true
+  validates :name, :address, presence: true
+
+  def self.search(search)
+    if search
+      where("name ILIKE?", "%#{search}%")
+    else
+      all
+    end
+  end
+
 end
