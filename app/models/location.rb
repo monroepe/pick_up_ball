9c50b2f5e82d9d6de0cc9d55905d1c8b1d10542e
Peter <monroepe@gmail.com>
2014-10-05 15:03:11 -0400

    Added validations for unique park name

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,8 +2,8 @@ class Location < ActiveRecord::Base
   belongs_to :user
   has_many :games, dependent: :destroy
 
+  validates :park_name, uniqueness: true
   validates :address, :park_name, presence: true
-
 
   def self.search(search)
     if search
