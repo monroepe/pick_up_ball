558d2487132e257581e3c9119cbb513626a43f85
Peter <monroepe@gmail.com>
2014-10-31 17:08:30 -0400

    Fixed seed file route

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-Hash.from_xml(File.read("nyc-courts.xml"))["basketball"]["facility"].inject({}) do |_result, elem|
+Hash.from_xml(File.read("db/nyc-courts.xml"))["basketball"]["facility"].inject({}) do |_result, elem|
   Location.create(name: elem["Name"], address: elem["Location"], city: "New York", state: "NY")
 end
