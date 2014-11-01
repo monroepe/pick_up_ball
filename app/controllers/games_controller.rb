a40972f8bb3feeeabcc8e7e2af09f7341c09c096
Peter <monroepe@gmail.com>
2014-11-01 14:18:28 -0400

    Added games to front page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,64 +1,65 @@
 class GamesController < ApplicationController
-before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
 
-def index
-  @games = current_user.games
-end
-
-def show
-  @game = Game.find(params[:id])
-  @players = @game.players
-  @comments = @game.comments.order("created_at")
-end
-
-def new
-  @location = Location.find(params[:location_id])
-  @game = Game.new
-end
-
-
-def create
-  @location = Location.find(params[:location_id])
-  @game = @location.games.build(game_params)
-  @game.user = current_user
-
-  if @game.save
-    @game.players.create(user_id: current_user.id)
-    redirect_to @location, notice: "Game created successfully!"
-  else
-    render "game/new"
+  def index
+    @games = current_user.games
   end
-end
 
-def edit
-  @location = Location.find(params[:location_id])
-  @game = @location.games.find(params[:id])
-end
+  def show
+    @game = Game.find(params[:id])
+    @players = @game.players.includes(:user)
+    @comments = @game.comments.order("created_at")
+  end
 
-def update
-  @location = Location.find(params[:location_id])
-  @game = @location.games.find(params[:id])
+  def new
+    @location = Location.find(params[:location_id])
+    @game = Game.new
+  end
 
-    if @game.update(game_params)
-      redirect_to @location, notice: "Game updated successfully"
+
+  def create
+    @location = Location.find(params[:location_id])
+    @game = @location.games.build(game_params)
+    @game.user = current_user
+
+    if @game.save
+      @game.players.create(user_id: current_user.id)
+      redirect_to @location, notice: "Game created successfully!"
     else
-      render "game/edit"
+      render "game/new"
     end
-end
+  end
 
-def destroy
-  @location = Location.find(params[:location_id])
-  @game = @location.games.find(params[:id])
+  def edit
+    @location = Location.find(params[:location_id])
+    @game = @location.games.find(params[:id])
+  end
 
-  @game.destroy
+  def update
+    @location = Location.find(params[:location_id])
+    @game = @location.games.find(params[:id])
 
-  redirect_to location_path(@location), notice: "Game destroyed successfully!"
-end
+      if @game.update(game_params)
+        redirect_to @location, notice: "Game updated successfully"
+      else
+        render "game/edit"
+      end
+  end
 
-private
-def game_params
-  params.require(:game).permit(:name, :time, :date, :description)
-end
+  def destroy
+    @location = Location.find(params[:location_id])
+    @game = @location.games.find(params[:id])
+
+    @game.destroy
+
+    redirect_to location_path(@location), notice: "Game destroyed successfully!"
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :time, :date, :description)
+  end
 
 end
 
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @games = Game.all.includes(:location)
   end
 end
