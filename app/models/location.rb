936681b368132a3e78954d31a6b77389158aec36
Peter <monroepe@gmail.com>
2014-11-02 18:06:38 -0500

    Test for creating location

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,8 +2,11 @@ class Location < ActiveRecord::Base
   belongs_to :user
   has_many :games, dependent: :destroy
 
+  STATES = %w(AL AK AZ AR CA CO CT DE DC FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TB TX UT VT VA WA WV WI WY AS GU MP PR VI)
+
   validates :name, uniqueness: true
-  validates :address, :name, presence: true
+  validates :address, :name, :city, :state, presence: true
+  validates :state, inclusion: { in: STATES, message: "must be real" }
 
   def self.search(search)
     if search
--- spec/features/locations/user_creates_location_spec.rb
+++ spec/features/locations/user_creates_location_spec.rb
@@ -10,7 +10,7 @@ feature "Create location" do
     fill_in "Name", with: "Court 1"
     fill_in "Address", with: "123 Fake St"
     fill_in "City", with: "New York"
-    fill_in "Address", with: "NY"
+    fill_in "State", with: "NY"
     click_on "Submit"
 
     expect(page).to have_content "Court created successfully!"
@@ -21,7 +21,79 @@ feature "Create location" do
     sign_in_as(user)
 
     visit new_location_path
+    click_on "Submit"
 
-    expect(page).to have_content "Court created successfully!"
+    expect(page).to have_content "can't be blank"
+  end
+
+  scenario "user fails to creates location succesfully - blank name" do
+    user = FactoryGirl.create(:user)
+    sign_in_as(user)
+
+    visit new_location_path
+
+    fill_in "Address", with: "123 Fake St"
+    fill_in "City", with: "New York"
+    fill_in "State", with: "NY"
+    click_on "Submit"
+
+    expect(page).to have_content "Name can't be blank"
+  end
+
+  scenario "user fails to creates location succesfully - blank address" do
+    user = FactoryGirl.create(:user)
+    sign_in_as(user)
+
+    visit new_location_path
+
+    fill_in "Name", with: "Court1"
+    fill_in "City", with: "New York"
+    fill_in "State", with: "NY"
+    click_on "Submit"
+
+    expect(page).to have_content "Address can't be blank"
+  end
+
+  scenario "user fails to creates location succesfully - blank city" do
+    user = FactoryGirl.create(:user)
+    sign_in_as(user)
+
+    visit new_location_path
+
+    fill_in "Name", with: "Court1"
+    fill_in "Address", with: "123 Fake St"
+    fill_in "State", with: "NY"
+    click_on "Submit"
+
+    expect(page).to have_content "City can't be blank"
+  end
+
+  scenario "user fails to creates location succesfully - blank state" do
+    user = FactoryGirl.create(:user)
+    sign_in_as(user)
+
+    visit new_location_path
+
+    fill_in "Name", with: "Court1"
+    fill_in "Address", with: "123 Fake St"
+    fill_in "City", with: "New York"
+    click_on "Submit"
+
+    expect(page).to have_content "State can't be blank"
+  end
+
+  scenario "user fails to creates location succesfully - fake state" do
+    user = FactoryGirl.create(:user)
+    sign_in_as(user)
+
+    visit new_location_path
+
+    fill_in "Name", with: "Court1"
+    fill_in "Address", with: "123 Fake St"
+    fill_in "City", with: "New York"
+    fill_in "State", with: "JK"
+    click_on "Submit"
+
+    expect(page).to have_content "State must be real"
   end
 end
